f87770f646d0237d72e7a5e7610edc20a4c85529
Jose Ordaz Suay <joseordazsuay@MBA02.local>
2026-06-15 00:30:21 +0200

    arreglar deploy

--- app/automations/page.tsx
+++ app/automations/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Link from 'next/link'
 import AutomationCard from '@/components/AutomationCard'
 import AnimatedReveal from '@/components/AnimatedReveal'
 import NewsletterForm from '@/components/NewsletterForm'
